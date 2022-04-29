24890de67b067ea3be5c89188bf6f5be63e28ab2
roaris <one0803kyopro@gmail.com>
2022-04-29 23:01:36 +0900

    プロジェクト全体のコンパイル方法

--- app.ts
+++ app.ts
@@ -1,16 +1,2 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = 'Max';
-
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-const result = generateError('エラーが発生しました', 500);
-console.log(result);
+const userName = 'aaa';
+console.log(userName);
