3d8323393ad4ec36a1a7ea44af54b320e735412c
roaris <one0803kyopro@gmail.com>
2022-04-30 07:29:09 +0900

    任意のパラメータ & プロパティ

--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,21 @@ add = function (n1, n2) {
 var Person = (function () {
     function Person(n) {
         this.age = 30;
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     Person.prototype.greet = function (phrase) {
-        console.log(phrase);
+        if (this.name) {
+            console.log(phrase + this.name);
+        }
+        else {
+            console.log('Hi');
+        }
     };
     return Person;
 }());
 var user1;
-user1 = new Person('Max');
+user1 = new Person();
 user1.greet('Hello');
 console.log(user1);
