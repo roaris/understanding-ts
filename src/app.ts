fbf385eaa11c71c9633165567d5b7f7f157f0360
roaris <one0803kyopro@gmail.com>
2022-04-30 00:17:36 +0900

    コード品質に寄与するオプション

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+let appId = 'abc';
 const button = document.querySelector('button')!;
 
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+function clickHandler(message: string) {
+  // let userName = 'Max';
+  console.log('Clicked! ' + message);
+}
+
 // a comment
-button.addEventListener('click', () => {
-  console.log('Clicked!');
-});
+button.addEventListener('click', clickHandler.bind(null, 'welcome'));
