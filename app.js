ad65fcdeb6d837e0dcf7b07ef28c0553d989e6bb
roaris <one0803kyopro@gmail.com>
2022-04-29 22:41:08 +0900

    never型

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError('エラーが発生しました', 500);
+console.log(result);
