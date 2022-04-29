0383caaada9106102eba4d0e0eb2fb5a4cf3cb43
roaris <one0803kyopro@gmail.com>
2022-04-29 21:47:09 +0900

    Tuple型

--- app.js
+++ app.js
@@ -3,14 +3,19 @@
 //   age: number;
 // }
 var person = {
-    name: 'aaa',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
+  name: 'aaa',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+  var hobby = _a[_i];
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map());
 }
