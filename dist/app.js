ec8fdf3bfaa2e47349f422766727776608ccfad3
roaris <one0803kyopro@gmail.com>
2022-05-02 00:26:49 +0900

    型キャスト

--- dist/app.js
+++ dist/app.js
@@ -61,3 +61,7 @@ function moveAnimal(animal) {
     console.log('移動速度: ' + speed);
 }
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+var userInputElement = document.getElementById('user-input');
+if (userInputElement) {
+    userInputElement.value = 'こんにちは';
+}
--- src/app.ts
+++ src/app.ts
@@ -100,3 +100,10 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+const userInputElement = document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'こんにちは';
+}
