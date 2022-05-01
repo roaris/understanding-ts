8dcc4363320de7f6ec2bea1a287b02d38eb8f9a9
roaris <one0803kyopro@gmail.com>
2022-05-02 00:08:14 +0900

    型ガード

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,55 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Privileges: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log('運転中...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('トラックを運転中...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('荷物を載せています...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(100);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
