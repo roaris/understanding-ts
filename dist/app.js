57b162287d7eaa7a7ac7df9212f4dccd8bf4b154
roaris <one0803kyopro@gmail.com>
2022-05-09 00:05:49 +0900

    Generic関数の作成

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,7 @@
 "use strict";
-var names = [];
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+var mergedObj = merge({ name: 'Max' }, { age: 30 });
+console.log(mergedObj.age);
+var mergedObj2 = merge({ name: 'Max' }, { age: 30 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
-const names: Array<string> = []; // string[]
-// names[0].split(' ');
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  // data.split(' ');
-});
+// promise.then((data) => {
+//   // data.split(' ');
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max' }, { age: 30 });
+console.log(mergedObj.age);
+const mergedObj2 = merge<{ name: string }, { age: number }>({ name: 'Max' }, { age: 30 });
