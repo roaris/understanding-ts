0361abc2344c3126c3ed2f2207338547c4568b42
roaris <one0803kyopro@gmail.com>
2022-05-08 23:52:49 +0900

    組み込みのGeneric型 & Genericsとは

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,2 @@
 "use strict";
-var e1 = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date(),
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-var result = add('Hello ', 'TypeScript');
-result.split(' ');
-var userInput = '';
-var storeData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(storeData);
+var names = [];
