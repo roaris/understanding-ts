bb4aa3d127a163b2c246becd955c452d8bba2a86
roaris <one0803kyopro@gmail.com>
2022-05-14 08:57:08 +0900

    Appコンポーネントに型を追加

--- sample-app/src/App.tsx
+++ sample-app/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function App() {
+// function App() {
+//   return <div className='App'></div>;
+// }
+
+// FC: Function Componentの略
+const App: React.FC = () => {
   return <div className='App'></div>;
-}
+};
 
 export default App;
